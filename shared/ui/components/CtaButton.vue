049fe14e0bf1a2aab2381229c472cacef4cd3851
Xifré Font <xifre.font.97@gmail.com>
2026-04-23 19:18:42 +0200

    feat(3): Add basic styles boilerplate

---
+++ shared/ui/components/CtaButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <button
+        class="cta-button"
+        type="button"
+    >
+        <slot />
+    </button>
+</template>
+
+<script lang="ts"></script>
+
+<style lang="scss">
+@use "#shared/ui/scss/index" as *;
+
+.cta-button {
+    cursor: pointer;
+    background-color: $colors-primary;
+}
+</style>
