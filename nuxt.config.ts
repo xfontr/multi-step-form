fa9b0f48ce068d370fca937c765b3f6ce8e8c1b3
Xifré Font <xifre.font.97@gmail.com>
2026-04-23 19:18:18 +0200

    feat(3): Install sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,15 @@ export default defineNuxtConfig({
 
     devtools: { enabled: false },
 
-    modules: ["@nuxt/eslint", "@nuxt/test-utils/module"],
+    modules: ["@nuxt/eslint", "@nuxt/test-utils/module", "@nuxtjs/i18n"],
+
+    i18n: {
+        defaultLocale: "es",
+        locales: [
+            { code: "es", name: "Spanish", file: "es.json" },
+            { code: "en", name: "English", file: "en.json" },
+        ],
+    },
+
+    css: ["#shared/ui/scss/index.scss"],
 });
