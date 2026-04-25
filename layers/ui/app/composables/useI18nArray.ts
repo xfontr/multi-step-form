5a6896a70e5512ce0e8193de886f808b3366d780
Xifré Font <xifre.font.97@gmail.com>
2026-04-25 13:37:33 +0200

    fix(16): i18n production different values

--- layers/ui/app/composables/useI18nArray.ts
+++ layers/ui/app/composables/useI18nArray.ts
@@ -4,13 +4,24 @@ type I18nArray = {
 
 function useI18nArray() {
     const i18n = useI18n();
+    const { env } = useRuntimeConfig().public;
+
+    function productionTm(key: string, maxLength?: number) {
+        const list = i18n.tm(key) ?? [];
+        return maxLength ? list.slice(0, maxLength) : (list as string[]);
+    }
 
     /**
      * @param key i18n key
      * @param maxLength Limits the length of the array. Recommended to prevent unexpected behaviour upon translation updates
      */
     function tm(key: string, maxLength?: number): string[] {
-        const list = (i18n.tm(key) as I18nArray).map(({ loc }) => loc.source);
+        if (env === "production") return productionTm(key, maxLength);
+
+        const list = ((i18n.tm(key) as I18nArray) ?? []).map(
+            ({ loc }) => loc?.source,
+        );
+
         return maxLength ? list.slice(0, maxLength) : list;
     }
 
