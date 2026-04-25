e5c91e844ab1df603e0b047567d666c85807c997
Xifré Font <xifre.font.97@gmail.com>
2026-04-25 18:48:05 +0200

    refactor(19): Add fallback group

--- layers/analytics/app/configs/constants.ts
+++ layers/analytics/app/configs/constants.ts
@@ -1,1 +1,8 @@
+import type { AnalyticsGroup } from "../types/AnalyticsGroup";
+
 export const MAX_COMPLETION = 100;
+
+export const DEFAULT_GROUP: AnalyticsGroup = {
+    name: "UNASSIGNED",
+    rate: 100,
+};
--- layers/analytics/app/stores/group.ts
+++ layers/analytics/app/stores/group.ts
@@ -1,15 +1,16 @@
 import type { AnalyticsGroup } from "../types/AnalyticsGroup";
+import { DEFAULT_GROUP } from "../configs/constants";
 
 const useGroupStore = defineStore(
     "group",
     () => {
         const { groups } = useRuntimeConfig().public.analytics;
 
-        const group = ref<AnalyticsGroup | undefined>(
-            pickAnalyticsGroup(groups as AnalyticsGroup[]),
+        const group = ref<AnalyticsGroup>(
+            pickAnalyticsGroup(groups as AnalyticsGroup[]) ?? DEFAULT_GROUP,
         );
 
-        return { group: readonly(group) };
+        return { group };
     },
     { persist: true },
 );
